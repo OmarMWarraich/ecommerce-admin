1b338e3449d14b766238692c18afb8300c25ea14
OmarMWarraich <earthling986@gmail.com>
2023-07-11 21:23:45 +0500

    style: shadecn-ui button

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <>
+      <p>Hello Admin DashBoard</p>    
+      <div className="p-4">
+        <Button size="default">Button</Button>
+      </div>
+      </>
+    )
+  }
+  
