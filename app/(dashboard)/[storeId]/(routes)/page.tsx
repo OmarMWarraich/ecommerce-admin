adb8f3145e1abcded06ee6b90b62cab62dfdf23d
OmarMWarraich <earthling986@gmail.com>
2023-07-12 11:17:23 +0500

    feat: Navigation Bar Admin

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,22 @@
-const DashboardPage = () => {
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+    params: { storeId: string }
+};
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+    
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId,
+        }
+    });
+
     return (
         <div>
-            <h1>Dashboard Page</h1>
-        </div>
+            Active Store: {store?.name}
+        </div> 
     )
 }
 
-export default DashboardPage
+export default DashboardPage;
